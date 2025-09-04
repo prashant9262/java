62851eaa7c4e5201a85d0c23c11e5fba80e19d66
prashant9262 <pandeyprashant552@gmail.com>
2025-09-04 14:56:29 +0530

    new

--- Loop/contineu_statment.java
+++ Loop/contineu_statment.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class contineu_ststment {
+public class contineu_statment {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
