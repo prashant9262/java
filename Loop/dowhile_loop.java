5ce02fd37836cb707e879713aaf4147e065a568e
prashant9262 <pandeyprashant552@gmail.com>
2025-09-04 11:35:39 +0530

    do while loop

---
+++ Loop/dowhile_loop.java
@@ -0,0 +1,11 @@
+public class dowhile_loop {
+    public static void main(String[] args) {
+        int counter = 1 ;
+        do{
+            System.out.println("hello world");
+            counter ++;
+        }while(counter<10);
+       
+    }
+    
+}
