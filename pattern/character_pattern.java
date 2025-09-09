bbe854893d684c0bc365e5ed3a51624bb20464aa
prashant9262 <pandeyprashant552@gmail.com>
2025-09-09 17:51:25 +0530

    characher patten

---
+++ pattern/character_pattern.java
@@ -0,0 +1,17 @@
+package pattern;
+
+public class character_pattern {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int n  = 4;
+        for (int i  = 1; i<=n; i++){
+            for(int j = 1; j <=i ; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+
+    }
+    
+}
