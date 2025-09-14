45970fcbb9e1131a7279342883e3e55258b5f3fd
prashant9262 <pandeyprashant552@gmail.com>
2025-09-14 15:40:30 +0530

    pattern and loop

---
+++ pattern/Hollow_Rectangle.java
@@ -0,0 +1,30 @@
+//package pattern;
+
+import java.util.Scanner;
+public class Hollow_Rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a row :");
+        int row = sc.nextInt();
+        System.out.println("enter a column :");
+        int column = sc.nextInt();
+
+        for (int i = 0; i<row; i++){
+            for(int j=0; j<column;j++){
+                if(i == 0 || i == row-1 || j == 0 || j == column-1)
+                System.out.print("*");
+                else{
+                    System.out.print(" ");
+                }
+
+
+            }
+           System.out.println( ); 
+            
+
+           
+        }
+       
+    }
+    
+}
