ec99569d231ea3771bd2ec221263bc8e99a74418
prashant9262 <pandeyprashant552@gmail.com>
2025-09-21 15:12:14 +0530

    comments in parameter

--- function_methodes/parameter.java
+++ function_methodes/parameter.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 public class parameter{
-    public static int calculatesum(int a, int b){
+    public static int calculatesum(int a, int b){ //jo parameters ham function ki defenation me likhte hai use ham --> parameter ya formal parameter kehte hai
         int sum = a + b;
         return sum;
     }
@@ -14,7 +14,7 @@ public class parameter{
         int a = sc.nextInt();
         System.out.print("Enter numer B:");
         int b = sc.nextInt();
-        int sum = calculatesum(a, b);
+        int sum = calculatesum(a, b); // jo value ham call krte hai wo ---> argument or actual parameter  hote hai
         System.out.println("Sum is :" + sum);
     }
     
