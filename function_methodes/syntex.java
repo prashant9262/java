a7ded154dc529c5a4f6c5f058319cd279f7e9e64
prashant9262 <pandeyprashant552@gmail.com>
2025-09-21 13:59:10 +0530

    Syntex of function

---
+++ function_methodes/syntex.java
@@ -0,0 +1,10 @@
+public class syntex {
+    public static void printhelloworld(){
+        System.out.println("Hello world");
+    }
+    public static void main(String[] args) {
+        printhelloworld();
+        
+    }
+    
+}
