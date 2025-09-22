954db831dbd8c36e46b170bb5a383310b344c379
prashant9262 <pandeyprashant552@gmail.com>
2025-09-23 00:57:06 +0530

    edit function overloading

---
+++ function_methodes/function_overloading.java
@@ -0,0 +1,14 @@
+public class function_overloading {
+    public static int sum_1(int a , int b){
+        return a + b;
+
+    }
+    public static int sum_2(int a, int b, int c){
+        return a + b + c;
+    }
+    public static void main(String[] args) {
+        System.out.println(sum_1(5, 4));
+        System.out.println(sum_2(4, 8, 6));
+    }
+    
+}
