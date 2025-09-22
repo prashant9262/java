496745e8305e9c415ab553bd2bd7dd8d3cb34eeb
prashant9262 <pandeyprashant552@gmail.com>
2025-09-22 23:45:11 +0530

    binomial_cofficient using methode

---
+++ function_methodes/binomial_cofficieant.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class binomial_cofficieant {
+    public static int factorial (int n){
+        int f = 1;
+
+        for (int i=1; i<=n; i++){
+            f = f*i;
+        }
+        return f;
+    }
+    public static int binocoff(int n, int r){
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n-r);
+
+        int binocoff = fact_n/(fact_r * fact_nmr);
+        return binocoff;
+
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number N :");
+        int n = sc.nextInt();
+        System.out.println( "Enter a number R :");
+        int r = sc.nextInt();
+        System.out.println("The factorial of "+ n + " or " + r + " " + " --> " + binocoff(n , r));
+
+        sc.close();
+    }
+
+    
+}
