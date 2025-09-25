5907f98873622568c8c0f8f730d4a055265a3c23
prashant9262 <pandeyprashant552@gmail.com>
2025-09-25 13:34:10 +0530

    optimize sollution

--- function_methodes/cheack_prime_or_not.java
+++ function_methodes/cheack_prime_or_not.java
@@ -1,21 +1,48 @@
 import java.util.Scanner;
 
+//public class cheack_prime_or_not {
+//     public static boolean isprime(int n){
+//         for(int i = 2; i<=n-1; i++){
+//             if (n%i == 0){
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a number :");
+//         int n = sc.nextInt();
+//         System.out.println(isprime(n));
+
+//         sc.close();
+//     }
+//}
+
+// this code for optmize solution to cheack if a number prime of using function and methode 
 public class cheack_prime_or_not {
     public static boolean isprime(int n){
-        for(int i = 2; i<=n-1; i++){
-            if (n%i == 0){
-                return false;
-            }
-        }
-        return true;
+    if(n == 2){
+      return true;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    for(int i = 2; i<=Math.sqrt(n); i++){
+        if (n % i == 0){
+            return false;
+        }
+
+    }
+    return true;
+
+ }
+ public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number :");
         int n = sc.nextInt();
         System.out.println(isprime(n));
 
         sc.close();
+    
     }
+
     
 }
