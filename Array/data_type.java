c56d706b45177aa49b481e3055a3ba6d6e3522a5
prashant9262 <pandeyprashant552@gmail.com>
2025-09-26 16:54:51 +0530

    Data Type in array

---
+++ Array/data_type.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class data_type {
+    // Data Type in array
+    
+    int marks [] = new int[50];
+    int number []= {1, 2, 3};
+    int morenumber[] = {1, 2, 3, 4};
+    String frute [] = {"apple", "mango", "banana"};
+    
+}
